1623080a212116facabca210b9da7e05dc2daa48
Fidel Maluto <fidel.maluto77@.com>
2026-04-29 02:01:48 +0100

    Instalando pacotes de validação

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -1,13 +1,40 @@
 const mongoose = require('mongoose');
 
 const LoginSchema = new mongoose.Schema({
-  titulo: { type: String, required: true },
-  descricao: String
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
 
 class Login {
+  constructor(body) {
+    this.body = body;
+    this.errors = [];
+    this.user = null;
+  }
+
+  register() {
+    this.valida();
+  }
+
+  valida() {
+    this.cleanUp();
+    
+  }
+
+  cleanUp() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
+        this.body[key];
+      }
+    }
+
+    this.body = {
+      email: this.body.email,
+      password: this.body.password
+    }
+  }
 
 }
 
