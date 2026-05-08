a7f2b3acf5e5ebf9fb04d11050ac314b58d51ac6
Fidel Maluto <fidel.maluto77@.com>
2026-05-08 20:40:18 +0100

    refactor: Melhorando a lógica dos controllers

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,9 @@
-const Login = require('../models/loginModel');
+const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login');
-}
+    if (req.session.user) return res.render('login-logado');
+    return res.render('login');
+};
 
 exports.register = async function (req, res) {
     try {
@@ -17,14 +18,41 @@ exports.register = async function (req, res) {
             return;
         }
 
-        req.flash('seccess', 'Usuário criado com sucesso!');
+        req.flash('success', 'Seu usuário foi criado com sucesso.');
         req.session.save(function () {
             return res.redirect('back');
         });
-        // return res.send(login.errors);
-    } catch (err) {
-        console.log(err);
+    } catch (e) {
+        console.log(e);
         return res.render('404');
     }
+};
 
-}
+exports.login = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('back');
+            });
+            return;
+        }
+
+        req.flash('success', 'Você entrou no sistema.');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('back');
+        });
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.logout = function (req, res) {
+    req.session.destroy();
+    res.redirect('/');
+};
