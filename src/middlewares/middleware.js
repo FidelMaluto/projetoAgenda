ea8ec2893390561ed1ac6ea8b3425bcab5c7befc
Fidel Maulto <fidel.maluto77@gmail.com>
2026-05-16 23:11:12 +0100

    refactor: Organizando o middleware

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -2,7 +2,9 @@ exports.middlewareGlobal = (req, res, next) => {
   res.locals.errors = req.flash('errors');
   res.locals.success = req.flash('success');
   res.locals.user = req.session.user;
+
   next();
+  
 };
 
 exports.outroMiddleware = (req, res, next) => {
@@ -10,24 +12,28 @@ exports.outroMiddleware = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if(err) {
+  if (err) {
     return res.render('404');
   }
 
   next();
+
 };
 
 exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
+  
   next();
+  
 };
 
 exports.loginRequired = (req, res, next) => {
-  if(!req.session.user) {
+  if (!req.session.user) {
     req.flash('errors', 'Você precisa fazer login.');
     req.session.save(() => res.redirect('/'));
     return;
   }
 
   next();
+  
 };
